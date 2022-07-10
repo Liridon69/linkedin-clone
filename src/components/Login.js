1f808042367bae73cb1b7c301027b50e697b2bba
donkO <lnikqi17@gmail.com>
2022-07-10 05:13:04 +0200

    Add & style image and text on login page main area

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,13 +5,24 @@ const Login = (props) => {
         <Container>
             <Nav>
                 <a href='/'>
-                    <img src='/images/login-logo.svg' alt=''></img>
+                    <img src='/images/login-logo.svg' alt=''/>
                 </a>
                 <div>
                     <Join>Join Now</Join>
                     <SignIn>Sign In</SignIn>
                 </div>
             </Nav>
+            <Section>
+                <Hero>
+                    <h1>Welcome to your professional community</h1>
+                    <img src='/images/login-hero.svg' alt=''/>
+                </Hero>
+                <Form>
+                    <Google>
+                        <img src='/images/google.svg' alt=''/>
+                    </Google>
+                </Form>
+            </Section>
         </Container>
     )
 };
@@ -31,7 +42,7 @@ const Nav = styled.nav`
     & > a {
         width:135px;
         height:34px;
-        @media (max-width: ;768px) {
+        @media (max-width:768px) {
             padding: 0 5px;
         }
     }
@@ -67,5 +78,65 @@ const SignIn = styled.a`
         text-decoration: none;
     }
 `;
+const Section = styled.section`
+    display: flex;
+    align-content: start;
+    min-height: 700px;
+    padding-bottom: 138px;
+    padding-top: 40px;
+    padding: 60px 0;
+    position: relative;
+    flex-wrap: wrap;
+    width: 100%;
+    max-width: 1128px;
+    align-items: center;
+    margin: auto;
+    @media (max-width:768px) {
+        margin: auto;
+        min-height: 0px;
+    }
+`;
+const Hero = styled.div`
+    width: 100%;
+    h1{
+        padding-bottom: 0;
+        width: 55%;
+        font-size: 56px;
+        color: #2977c9;
+        font-weight: 200;
+        line-height: 70px;
+        @media (max-width: 768px) {
+            text-align: center;
+            font-size: 20px;
+            width: 100%;
+            line-height: 2;
+        }
+    }
+    img{
+        /* z-index: -1; */
+        width: 700px;
+        height: 670px;
+        position: absolute;
+        bottom: -2px;
+        right: -150px;
+        @media (max-width:768px) {
+            top: 230px;
+            width: initial;
+            position: initial;
+            height: initial;
+        }
+    }
+`;
 
+const Form = styled.div`
+    margin-top: 100px;
+    width: 488px;
+    @media (max-width:768px) {
+        margin-top: 20px;
+    }
+`;
+
+const Google = styled.button`
+
+`;
 export default Login;
