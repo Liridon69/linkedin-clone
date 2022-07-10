46540af044cd17aaec2e4574e7c69b532b5b40ae
donkO <lnikqi17@gmail.com>
2022-07-10 04:30:11 +0200

    README

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components'
 
 const Login = (props) => {
-    return <div>Login</div>;
+    return (
+        <Container>
+            <Nav>
+                <a href='/'>
+                    <img src='/images/login-logo.svg' alt=''></img>
+                </a>
+            </Nav>
+        </Container>
+    )
 };
+
+const Container = styled.div``;
+const Nav = styled.nav``;
 export default Login;
