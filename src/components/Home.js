5a588b81db1e72d759c30e4eb1f52f4095c0a3d1
donkO <lnikqi17@gmail.com>
2022-07-11 19:47:52 +0200

    Create Home Screen section structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Leftside from "./Leftside";
+import Main from "./Main";
+import Rightside from "./Rightside";
 
 const Home = (props) => {
     return (
@@ -11,6 +14,11 @@ const Home = (props) => {
                 Find talented pros in record time with Upwork and keep business moving.
                 </p>
             </Section>
+            <Layout>
+                <Leftside />
+                <Main />
+                <Rightside />
+            </Layout>
         </Container>
     );
 
@@ -50,5 +58,18 @@ const Section = styled.section`
         padding: 0 5px;
     }
 `;
-
+const Layout = styled.div`
+    display: grid;
+    grid-template-areas: "leftside main rightside";
+    grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+    column-gap: 25px;
+    row-gap: 25px;
+    /* grid-template-rows: auto; */
+    margin: 25px 0;
+    @media (max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        padding: 0 5px;
+    }
+`;
 export default Home;
